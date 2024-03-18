d5bdb89c1260d69f76f467344b6d42146c787e01
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-03-18 04:32:11 +0200

    18.03

--- src/Components/Main/Section_5/Section_5.jsx
+++ src/Components/Main/Section_5/Section_5.jsx
@@ -1,5 +1,43 @@
-import "./Section_5.scss"
+import React, { useState } from 'react';
+import './Section_5.scss';
+
 function Section_5() {
+    const [activeReviewIndex, setActiveReviewIndex] = useState(0);
+
+    const reviews = [
+        {
+            stars: 4,
+            name: 'Жора Ремінгтон',
+            content: 'Дуже задоволений якістю обслуговування і самою продукцією...',
+        },
+        {
+            stars: 4,
+            name: 'Жора Ремінгтон',
+            content: 'Дуже задоволений якістю обслуговування і самою продукцією...',
+        },
+        {
+            stars: 4,
+            name: 'Жора Ремінгтон',
+            content: 'Дуже задоволений якістю обслуговування і самою продукцією...',
+        },
+        {
+            stars: 4,
+            name: 'Жора Ремінгтон',
+            content: 'Дуже задоволений якістю обслуговування і самою продукцією...',
+        },
+
+
+    ];
+
+    const nextReview = () => {
+        setActiveReviewIndex(prevIndex => (prevIndex === reviews.length - 1 ? 0 : prevIndex + 1));
+        console.log(activeReviewIndex)
+    };
+
+    const prevReview = () => {
+        setActiveReviewIndex(prevIndex => (prevIndex === 0 ? reviews.length - 1 : prevIndex - 1));
+        console.log(activeReviewIndex)
+    };
 
     return (
         <section className='section5'>
@@ -13,56 +51,27 @@ function Section_5() {
                             <p>03</p>
                         </div>
                         <div className='section5ContentText2_2'>
-                            <p>&#10229;</p>
-                            <p>&#10230;</p>
+                            <button onClick={prevReview}>&#10229;</button>
+                            <button onClick={nextReview}>&#10230;</button>
                         </div>
                     </div>
                 </div>
-                <div className='section5ContentCarts'>
-                    <div className='section5ContentCart section5ContentCart1'>
-                        <div className='section5ContentCartName section5ContentCart1Name'>
-                            <p>&#x2605; &#x2605; &#x2605; &#x2605;  &#x2606;</p>
-                            <p>Жора Ремінгтон</p>
+                <div className='section5ContentCarts'  style={{
+                    transform: `translateX(-${activeReviewIndex * 1170/4}px)`
+
+                }}>
+                    {reviews.map((review, index) => (
+                        <div key={index} className={`section5ContentCart section5ContentCart${index + 1}`}>
+                            <div className='section5ContentCartName section5ContentCart1Name'>
+                                <p>&#x2605; &#x2605; &#x2605; &#x2605;  &#x2606;</p>
+                                <p>{review.name}</p>
+                            </div>
+                            <p className='section5ContentCartRewiew'>{review.content}</p>
                         </div>
-                        <p className='section5ContentCartRewiew'>
-                            Дуже задоволений якістю обслуговування і самою продукцією. Купували термобілизну у
-                            подарунок, друзі...
-                        </p>
-                    </div>
-                    <div className='section5ContentCart section5ContentCart2'>
-                        <div className='section5ContentCartName section5ContentCart2Name'>
-                            <p>&#x2605; &#x2605; &#x2605; &#x2605;  &#x2606;</p>
-                            <p>Жора Ремінгтон</p>
-                        </div>
-                        <p className='section5ContentCartRewiew'>
-                            Дуже задоволений якістю обслуговування і самою продукцією. Купували термобілизну у
-                            подарунок, друзі...
-                        </p>
-                    </div>
-                    <div className='section5ContentCart section5ContentCart3'>
-                        <div className='section5ContentCartName section5ContentCart3Name'>
-                            <p>&#x2605; &#x2605; &#x2605; &#x2605;  &#x2606;</p>
-                            <p>Жора Ремінгтон</p>
-                        </div>
-                        <p className='section5ContentCartRewiew'>
-                            Дуже задоволений якістю обслуговування і самою продукцією. Купували термобілизну у
-                            подарунок, друзі...
-                        </p>
-                    </div>
-                    <div className='section5ContentCart section5ContentCart4'>
-                        <div className='section5ContentCartName section5ContentCart4Name'>
-                            <p>&#x2605; &#x2605; &#x2605; &#x2605;  &#x2606;</p>
-                            <p>Жора Ремінгтон</p>
-                        </div>
-                        <p className='section5ContentCartRewiew'>
-                            Дуже задоволений якістю обслуговування і самою продукцією. Купували термобілизну у
-                            подарунок, друзі...
-                        </p>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
-    )
+    );
 }
-
-export default Section_5
+export default Section_5;
