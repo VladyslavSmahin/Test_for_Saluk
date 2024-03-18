5b6ed1c5b3d7473cbbd1e713428b70448f565949
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-03-18 04:55:40 +0200

    18.03

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,7 +2,7 @@ import Section_1 from "./Section_1/Section_1.jsx";
 import Section_2 from "./Section_2/Section_2.jsx";
 import Section_3 from "./Section_3/Section_3.jsx";
 import Section_4 from "./Section_4/Section_4.jsx";
-/*import Section_5 from "./Section_5/Section_5.jsx";*/
+import Section_5 from "./Section_5/Section_5.jsx";
 
 function Main() {
 
@@ -12,7 +12,7 @@ function Main() {
             <Section_2/>
             <Section_3/>
             <Section_4/>
-           {/* <Section_5/>*/}
+            <Section_5/>
         </>
     )
 }
